fe750b00a3b8786de5debbc0b646d0b645ddcf7d
scotty <47952420+davetoshi@users.noreply.github.com>
2019-09-11 17:30:03 +0100

    update p2pool deploy
    updated script
    added header information
    added config.js for header

--- p2pool.deploy.sh
+++ p2pool.deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Author: Chris Har, AXErunners
+# Author: davetoshi, AXErunners
 # Thanks to all who published information on the Internet!
 #
 # Disclaimer: Your use of this script is at your sole risk.
@@ -11,15 +11,11 @@
 # script, whether direct, indirect, special, incidental or consequential, even if it
 # has been advised of the possibility of such damages.
 #
-# NOTE:
-# This script is based on:
-# - Git Commit: 18dc987 => https://github.com/dashpay/p2pool-dash
-# - Git Commit: 20bacfa => https://github.com/dashpay/dash
-#
+# 
 # You may have to perform your own validation / modification of the script to cope with newer
 # releases of the above software.
 #
-# Tested with Ubuntu 17.10
+# Tested with Ubuntu 18.04
 #
 cat << "EOF"
     ______     __  __     ______
@@ -42,23 +38,44 @@ PUBLIC_IP=<your public IP address>
 EMAIL=<your email address>
 PAYOUT_ADDRESS=<your AXE wallet address to receive fees>
 USER_NAME=<linux user name>
-RPCUSER=<your random rpc user name>
-RPCPASSWORD=<your random rpc password>
+Location=<pool location country or state>
 
 FEE=0.5
 DONATION=0.5
-AXE_WALLET_URL=https://github.com/AXErunners/axe/releases/download/v1.4.0/axecore-1.4.0-x86_64-linux-gnu.tar.gz
-AXE_WALLET_ZIP=axecore-1.4.0-x86_64-linux-gnu.tar.gz
+AXE_WALLET_URL=https://github.com/AXErunners/axe/releases/download/v1.4.0.2/axecore-1.4.0.2-x86_64-linux-gnu.tar.gz
+AXE_WALLET_ZIP=axecore-1.4.0.2-x86_64-linux-gnu.tar.gz
 AXE_WALLET_LOCAL=axecore-1.4.0
 P2POOL_FRONTEND=https://github.com/justino/p2pool-ui-punchy
-P2POOL_FRONTEND2=https://github.com/johndoe75/p2pool-node-status
-P2POOL_FRONTEND3=https://github.com/hardcpp/P2PoolExtendedFrontEnd
+P2POOL_FRONTEND2=https://github.com/hardcpp/P2PoolExtendedFrontEnd
 
 #
+sudo apt update
+sudo apt upgrade
+sudo apt install ufw git fail2ban python virtualenv unzip pv
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw allow 9937/tcp
+sudo ufw allow 9936/tcp
+sudo ufw allow 9337/tcp
+sudo ufw allow 7923/tcp
+sudo ufw allow 8999/tcp
+sudo ufw logging on
+sudo ufw disable
+sudo ufw enable
+
+
+
+#install axe
+
+git clone https://github.com/axerunners/axerunner
+~/axerunner/axerunner install
+~/axerunner/axerunner install sentinel
+~/axerunner/axerunner restart now
 # Install Prerequisites
 #
 cd ~
-sudo apt-get --yes install python-zope.interface python-twisted python-twisted-web python-dev
+sudo apt-get --yes install python-zope.interface python-twisted python-twisted-web python-dev libncurses-dev
+sudo apt-get --yes install git python-zope.interface python-twisted python-twisted-web
 sudo apt-get --yes install gcc g++
 sudo apt-get --yes install git
 
@@ -82,57 +99,65 @@ mv web-static web-static.old
 git clone $P2POOL_FRONTEND web-static
 mv web-static.old web-static/legacy
 cd web-static
-git clone $P2POOL_FRONTEND2 status
-git clone $P2POOL_FRONTEND3 ext
+git clone $P2POOL_FRONTEND2 ext
 
 #
-# Get specific version of AXE wallet for Linux
+# create config.js
 #
-cd ~
-mkdir axe
-cd axe
-wget $AXE_WALLET_URL
-tar -xvzf $AXE_WALLET_ZIP
-rm $AXE_WALLET_ZIP
 
-#
-# Copy AXE daemon
-#
-sudo cp ~/axe/$AXE_WALLET_LOCAL/bin/axed /usr/bin/axed
-sudo cp ~/axe/$AXE_WALLET_LOCAL/bin/axe-cli /usr/bin/axe-cli
-sudo chown -R $USER_NAME:$USER_NAME /usr/bin/axed
-sudo chown -R $USER_NAME:$USER_NAME /usr/bin/axe-cli
-
-#
-# Prepare AXE configuration
-#
-mkdir ~/.axecore
-cat <<EOT >> ~/.axecore/axe.conf
-rpcuser=$RPCUSER
-rpcpassword=$RPCPASSWORD
-alertnotify=echo %s | mail -s "AXE Alert" $EMAIL
-server=1
-daemon=1
+cat <<EOT >> ~/git/p2pool-axe/web-static/js/config.js
+var config = {
+  myself: [
+    "1MzFr1eKzLEC1tuoZ7URMB7WWBMgHKimKe",
+    "LSRfZJf75MtwzrbAUfQgqzdK4hHpY4oMW3"
+  ],
+  host: "http://$PUBLIC_IP:7923",
+  // data reload interval in seconds
+  reload_interval: 30,
+  // chart reload interval in seconds
+  reload_chart_interval: 600,
+  // HTML to load at top of page
+  header_content_url: "http://$PUBLIC_IP:7923/static/hello.html",
+  // Default Theme
+  theme: 'cyborg'
+}
 EOT
 
 #
-# Get latest AXE core
+#create page header
 #
-cd ~/git
-git clone https://github.com/axerunners/axe
+cat <<EOT >> ~/git/p2pool-axe/web-static/hello.html
+<head>
+  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+  <title>AXE p2pool - Welcome</title>
+ </head>
+ <body>
+  <big style="font-weight: bold;"><big>Welcome to AXE p2pool</big></big><br>
+        <section class="panel panel-default clearfix">
+                <div class="panel-heading">
+                        <h4>$Location Node</h4>
+                </div>
+                <ul class="list-group status info_one pull-left">
+                        <li class="list-group-item"><center><br><img src="https://raw.githubusercontent.com/AXErunners/media/master/axerunners-x-100.png"></img>
+                                <br>
+				<br>AXE Core version 1.4.0.2
+                                <br><a href="https://axerunners.com/" target="_blank">axerunners.com</a><br$
+                        </li>
+                </ul>
+                <ul class="list-group status info_two pull-right">
+                        <li class="list-group-item"><br>Use the following information to get started:<br>
+                                <br>&nbsp; &nbsp; Pool URL:&nbsp; &nbsp; &nbsp;<span>stratum+tcp://$PUBLIC_IP:7923</span>
+                                <br>&nbsp; &nbsp; User ID:&nbsp; &nbsp; &nbsp; &nbsp;<span>Use your AXE wallet address</span>
+                                <br>&nbsp; &nbsp; Password:&nbsp; &nbsp;<span>Use any password (ignored by pool)</span>
+                                <center><br><span><b>Do not mine directly to an exchange address!</b></span><br></center>
+                        </li>
+                </ul>
+        </section>
+  </body>
+</html>
+EOT
 
-#
-# Install AXE daemon service and set to Auto Start
-#
-cd /etc/systemd/system
-sudo ln -s /home/$USER_NAME/git/axe/contrib/init/axed.service axed.service
-sudo sed -i 's/User=axecore/User='"$USER_NAME"'/g' axed.service
-sudo sed -i 's/Group=axecore/Group='"$USER_NAME"'/g' axed.service
-sudo sed -i 's/\/var\/lib\/axed/\/home\/'"$USER_NAME"'\/.axecore/g' axed.service
-sudo sed -i 's/\/etc\/axecore\/axe.conf/\/home\/'"$USER_NAME"'\/.axecore\/axe.conf/g' axed.service
-sudo systemctl daemon-reload
-sudo systemctl enable axed
-sudo service axed start
+
 
 #
 # Prepare p2pool startup script
@@ -145,11 +170,17 @@ if [ $? -eq 0 ]
 then
 echo
 echo Installation Completed.
-echo You can start p2pool instance by command:
+echo Now wait till axe runner has finished sync
+echo
+echo axerunner/axerunner status
+echo 
+echo once sync is complete aprox 10 mins
+echoYou can start p2pool instance by command:
+echo start screen
 echo
 echo bash ~/p2pool.start.sh
-echo
-echo NOTE: you will need to wait until AXE daemon has finished
+echo 
+echo NOTE: you will need to wait until AXE runner has finished
 echo blockchain synchronization before the p2pool instance is usable.
 echo
 fi
